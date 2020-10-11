96f85c51f86980b4f8c0c5ea89b1c4b1d00a7b24
kiramipt <U098iolk>
2020-10-12 02:05:35 +0300

    add encode to tests.py

--- tests.py
+++ tests.py
@@ -9,7 +9,7 @@ class FlaskAppTests(unittest.TestCase):
 
     def test_get(self):
         r = self.app.get('/')
-        self.assertEqual(r.data, 'У меня получилось!')
+        self.assertEqual(r.data, 'У меня получилось!'.encode('utf-8'))
 
     def test_post(self):
         r = self.app.post('/')
